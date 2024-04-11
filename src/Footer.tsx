3f503a7ee1c3f75d5b55d2269bbb75af7973c465
RyanLindman <ryan.lindman@outlook.com>
2024-04-11 21:48:56 +0200

    progress on cards for services, not dynamic but suits the purpose for demo

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+import { Box, Grid } from '@mui/material'
 import React from 'react'
 import Breadcrumbs from './Breadcrumbs'
 import InstagramIcon from '@mui/icons-material/Instagram'
@@ -7,49 +7,51 @@ import MailOutlineIcon from '@mui/icons-material/MailOutline'
 import PhoneIcon from '@mui/icons-material/Phone'
 
 const Footer = () => {
-  return (
-    <Box
-    sx={{
-      width: '100%',
-      height: '200px',
-      background: '#ccc2',
-      borderTopLeftRadius: '10px',
-      borderTopRightRadius: '10px',
-      display: 'flex',
-      justifyContent: 'center',
-      flexDirection: 'column',
-      alignItems: 'center',
-      gap: '30px',
-        
-    }}
-  >
-    <Box>
-      <Breadcrumbs />
-    </Box>
-    <Box sx={{ display: 'flex', gap: '15px', color: '#ccc' }}>
-      <a
-        href="https://instagram.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <InstagramIcon />
-      </a>
-      <a
-        href="https://facebook.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FacebookIcon />
-      </a>
-      <a>
-        <MailOutlineIcon />
-      </a>
-      <a>
-        <PhoneIcon />
-      </a>
-    </Box>
-  </Box>
-  )
+   return (
+      <Grid container pl={2} pr={2}>
+         <Box
+            sx={{
+               width: '100%',
+               height: '200px',
+               background: '#ccc2',
+               borderTopLeftRadius: '10px',
+               borderTopRightRadius: '10px',
+               display: 'flex',
+               justifyContent: 'center',
+               flexDirection: 'column',
+               alignItems: 'center',
+               gap: '30px',
+               mt: 15,
+            }}
+         >
+            <Box>
+               <Breadcrumbs />
+            </Box>
+            <Box sx={{ display: 'flex', gap: '15px', color: '#ccc' }}>
+               <a
+                  href="https://instagram.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+               >
+                  <InstagramIcon />
+               </a>
+               <a
+                  href="https://facebook.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+               >
+                  <FacebookIcon />
+               </a>
+               <a>
+                  <MailOutlineIcon />
+               </a>
+               <a>
+                  <PhoneIcon />
+               </a>
+            </Box>
+         </Box>
+      </Grid>
+   )
 }
 
 export default Footer
--- src/views/services/Content.tsx
+++ src/views/services/Content.tsx
@@ -1,120 +1,135 @@
 import {
-  Paper,
-  Grid,
-  ThemeProvider,
-  Typography,
-  createTheme,
-  Container,
-  Box,
-  Grow,
+   Paper,
+   Grid,
+   ThemeProvider,
+   Typography,
+   createTheme,
+   Container,
+   Box,
+   Grow,
+   Card,
 } from '@mui/material'
 import './content.css'
 import { useState } from 'react'
 import Map from '../../Map'
+import SnowCard from './SnowCard'
+import MarineCard from './MarineCard'
+import GravelCard from './GravelCard'
 
 const Content = () => {
-  const theme = createTheme({
-    components: {
-      MuiGrid: {
-        styleOverrides: {
-          
-        },
+   const theme = createTheme({
+      components: {
+         MuiGrid: {
+            styleOverrides: {},
+         },
+         MuiPaper: {
+            styleOverrides: {
+               elevation3: {
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  padding: 20,
+                  marginTop: 10,
+               },
+            },
+         },
+         MuiTypography: {
+            styleOverrides: {
+               h5: {
+                  textTransform: 'uppercase',
+                  fontWeight: 700,
+                  fontFamily: 'Montserrat',
+               },
+            },
+         },
+         MuiContainer: {
+            styleOverrides: {
+               disableGutters: {},
+            },
+         },
       },
-      MuiPaper: {
-        styleOverrides: {
-          elevation3: {
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',          
-            padding: 20,
-            border: '1px solid black',
-          },
-          
-        },
-      },
-      MuiTypography: {
-        styleOverrides: {
-          h5: {
-            textTransform: 'uppercase',
-            fontWeight: 700,
-            fontFamily: 'Montserrat',
-          },
-        },
-      },
-      MuiContainer: {
-        styleOverrides: {
-          disableGutters: {},
-        },
-      },
-    },
-  })
+   })
 
-  const [checked, setChecked] = useState(true)
+   const [checked, setChecked] = useState(true)
 
-  return (
-    <>
-      <Box mt={10}>
-        <Box>
-          {checked && (
-            <Grow timeout={'auto'} in={checked}>
-              <div>
-                {/* Render content if checked is true */}
-                <ThemeProvider theme={theme}>
-                  <Container maxWidth={false}>
-                    <Box display={'flex'} pt={2} pb={2}>
-                      <Typography
-                        textAlign={'start'}
-                        mt={10}
-                        variant="h2"
-                        color={'white'}
-                        padding={5}
-                        fontFamily={'Montserrat'}
-                      >
-                        Vi utför sommar och vinterunderhåll <br /> inom Västra
-                        Götaland
-                      </Typography>
-                      <Map />
-                    </Box>
-                    <Grid
-                      container
-                      spacing={2}
-                      sx={{
-                        height: 'auto',
-                        justifyContent: 'center',
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                      }}
-                    >
-                      <Grid item xs={6}>
-                        <Paper elevation={3}>
-                          <Typography variant="h5">Snöröjning</Typography>
-                        </Paper>
-                      </Grid>
-                      <Grid item xs={6}>
-                        <Paper elevation={3}>
-                          <Typography variant="h5">Grus & städning</Typography>
-                        </Paper>
-                      </Grid>
-                      <Grid item xs={6}>
-                        <Paper elevation={3}>
-                          <Typography variant="h5">Tvätt</Typography>
-                        </Paper>
-                      </Grid>
-                      <Grid item xs={6}>
-                        <Paper elevation={3}>
-                          <Typography variant="h5">Båt & Marin</Typography>
-                        </Paper>
-                      </Grid>
-                    </Grid>
-                  </Container>
-                </ThemeProvider>
-              </div>
-            </Grow>
-          )}
-        </Box>
-      </Box>
-    </>
-  )
+   return (
+      <>
+         <Box mt={10}>
+            <Box>
+               {checked && (
+                  <Grow timeout={'auto'} in={checked}>
+                     <div>
+                        {/* Render content if checked is true */}
+                        <ThemeProvider theme={theme}>
+                           <Container maxWidth={false}>
+                              <Box display={'flex'} pt={2} pb={2}>
+                                 <Typography
+                                    textAlign={'start'}
+                                    mt={10}
+                                    variant="h2"
+                                    color={'white'}
+                                    padding={5}
+                                    fontFamily={'Montserrat'}
+                                 >
+                                    Vi utför sommar och vinterunderhåll <br />{' '}
+                                    inom Västra Götaland
+                                 </Typography>
+                                 <Map />
+                              </Box>
+                              <Grid
+                                 container
+                                 spacing={3}
+                                 sx={{
+                                    height: 'auto',
+                                    justifyContent: 'center',
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                 }}
+                              >
+                                 <Grid item xs={6}>
+                                    <Paper
+                                       elevation={3}
+                                       sx={{
+                                          background:
+                                             'linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(215,222,255,1) 95%)',
+                                       }}
+                                    >
+                                       <SnowCard />
+                                    </Paper>
+                                 </Grid>
+                                 <Grid item xs={6}>
+                                    <Paper
+                                       elevation={3}
+                                       sx={{
+                                          background:
+                                             'linear-gradient(90deg, rgba(215,222,255,1) 5%, rgba(255,255,255,1) 100%)',
+                                       }}
+                                    >
+                                       <GravelCard />
+                                    </Paper>
+                                 </Grid>
+                                 <Grid item xs={6}>
+                                    <Paper elevation={3}>
+                                       <Typography variant="h5">
+                                          Tvätt
+                                       </Typography>
+                                    </Paper>
+                                 </Grid>
+                                 <Grid item xs={6}>
+                                    <Paper elevation={3}>
+                                       <MarineCard />
+                                    </Paper>
+                                 </Grid>
+                              </Grid>
+                           </Container>
+                        </ThemeProvider>
+                     </div>
+                  </Grow>
+               )}
+            </Box>
+         </Box>
+      </>
+   )
 }
 
 export default Content
